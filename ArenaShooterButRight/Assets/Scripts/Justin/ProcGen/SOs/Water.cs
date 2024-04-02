1653826f448372027ff8c2304a04f01fb11d39c4
Zeny97 <JustinSontopski@outlook.de>
2024-04-02 17:27:30 +0200

    Map Generation
    Noise Skript
    MapGenerator Skript
    MapDisplay Skript
    
    TerrainType Scriptable Object.
    Editor Tool zum generieren von NoiseMaps

---
+++ ArenaShooterButRight/Assets/Scripts/Justin/ProcGen/SOs/Water.cs
@@ -0,0 +1,10 @@
+using Justin.ProcGen.SOs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/JustinGen/TerrainType/Water")]
+public class Water : ScriptableObject
+{
+
+}
