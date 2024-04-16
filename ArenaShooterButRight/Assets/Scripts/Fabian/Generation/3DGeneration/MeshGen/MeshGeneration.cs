df52f068d5cb0801394a1ca899ed2acc3c8b0fab
Fabian Wittgen <wittgenfabian@gmail.com>
2024-04-16 15:05:57 +0200

    re-done generation

---
+++ ArenaShooterButRight/Assets/Scripts/Fabian/Generation/3DGeneration/MeshGen/MeshGeneration.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Fabian.Generation._3DGeneration.MeshGen
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))] [ExecuteInEditMode]
+
+    public class MeshGeneration : MonoBehaviour
+    {
+        [SerializeField] public int xSize;
+        [SerializeField] public int ySize;
+        private Vector3[] _vertices;
+        private Mesh _mesh;
+
+        public void Start()
+        {
+            GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
+            _mesh.name = "Procedural Grid";
+            Generate();
+        }
+
+        private void Generate()
+        {
+        
+            _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+            Vector2[] uv = new Vector2[_vertices.Length];
+            
+            for (int i = 0, y=0; y <= ySize; y++)
+            {
+                for (int x = 0; x <= xSize; x++, i++)
+                {
+                    _vertices[i] = new Vector3(x,0, y);
+                    uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                }
+            }
+            
+            _mesh.vertices = _vertices;
+            _mesh.uv = uv;
+
+            int[] triangles = new int[xSize * ySize * 6];
+
+            for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+            {
+                for (int x = 0; x < xSize; x++, ti += 6, vi++)
+                {
+                    triangles[ti] = vi;
+                    triangles[ti + 1] = triangles[ti + 4] = vi + xSize + 1;
+                    triangles[ti + 2] = triangles[ti + 3] = vi + 1;
+                    triangles[ti + 5] = vi + xSize + 2;
+                }
+            }
+            _mesh.triangles = triangles;
+            _mesh.RecalculateNormals();
+        }
+    }
+}
