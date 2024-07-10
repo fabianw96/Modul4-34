8e186c95ee2c010ff23503ca4e46c58f79a4e4ae
Fabian Wittgen <wittgenfabian@gmail.com>
2024-07-10 19:14:08 +0200

    done diddle did some gpu instancing stuff.

--- ArenaShooterButRight/Assets/Scripts/Fabian/EngineTool/PrefabSpawner/PrefabSpawner.cs
+++ ArenaShooterButRight/Assets/Scripts/Fabian/EngineTool/PrefabSpawner/PrefabSpawner.cs
@@ -10,15 +10,21 @@ using Random = UnityEngine.Random;
 
 public class PrefabSpawner : EditorWindow
 {
-    private List<GameObject> _prefabChoiceLst;
-    private List<GameObject> _spawnedPrefabs;
+    //Lists for prefabs
+    private List<GameObject> _prefabChoiceLst = new();
+    private List<GameObject> _spawnedPrefabs = new();
+    
+    //Visual UI Elements
     private VisualElement _root;
     private Label _label;
     private DropdownField _prefabDropDown;
     private Toggle _enableButton;
     private Button _clearButton;
-    private LayerMask _terrainLayer;
+    private Slider _radiusSlider;
     
+    //Values
+    private float _radius;
+
 
     [MenuItem("Window/UI Toolkit/PrefabSpawner")]
     public static void ShowExample()
@@ -29,14 +35,13 @@ public class PrefabSpawner : EditorWindow
 
     public void CreateGUI()
     {
-        _terrainLayer = LayerMask.GetMask("Terrain");
-        
-        SceneView.duringSceneGui += OnClick;
+        SceneView.duringSceneGui += EvaluateMousePosition;
 
         _root = rootVisualElement;
         _label = new Label("PrefabSpawner");
         DrawReorderableList(_prefabChoiceLst, rootVisualElement);
         _enableButton = new Toggle("Enable spawning");
+        _radiusSlider = new Slider(0f, 10f);
         _clearButton = new Button()
         {
             text = "Clear spawned prefabs!",
@@ -45,7 +50,26 @@ public class PrefabSpawner : EditorWindow
         
         _root.Add(_label);
         _root.Add(_enableButton);
+        _root.Add(_radiusSlider);
         _root.Add(_clearButton);
+    }
+    
+    private void EvaluateMousePosition(SceneView obj)
+    {
+        Vector3 mousePos = Event.current.mousePosition;
+        float pixelsPerPoint = EditorGUIUtility.pixelsPerPoint;
+        mousePos.y = obj.camera.pixelHeight - mousePos.y * pixelsPerPoint;
+        mousePos.x *= pixelsPerPoint;
+        
+        Ray ray = obj.camera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        _radius = _radiusSlider.value;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            DrawCircle(hit);
+            DrawPrefabs(hit);
+        }
     }
 
     private void ClearSpawnedObjects(ClickEvent evt)
@@ -58,28 +82,28 @@ public class PrefabSpawner : EditorWindow
         _spawnedPrefabs.Clear();
     }
 
-    private void OnClick(SceneView obj)
+    private void DrawCircle(RaycastHit hit)
     {
-        Event e = Event.current;
+
+        if (!_enableButton.value)
+            return;
+
+        Handles.color = Color.green;
+        Handles.CircleHandleCap(0,hit.point,Quaternion.Euler(90f, 0f, 0f), _radius, EventType.Repaint);
+    }
+
+    private void DrawPrefabs(RaycastHit hit)
+    {
+        if (!_enableButton.value || Event.current.type != EventType.MouseDrag) 
+            return;
         
-        if (_enableButton.value && e.type == EventType.MouseDrag)
-        {
-            GUIUtility.hotControl = 1;
-            Vector3 mousePos = e.mousePosition;
-            float pixelsPerPoint = EditorGUIUtility.pixelsPerPoint;
-            mousePos.y = obj.camera.pixelHeight - mousePos.y * pixelsPerPoint;
-            mousePos.x *= pixelsPerPoint;
-
-            Ray ray = obj.camera.ScreenPointToRay(mousePos);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, _terrainLayer))
-            {
-                int randomChoice = Random.Range(0, _prefabChoiceLst.Count);
-                GameObject o = Instantiate(_prefabChoiceLst[randomChoice], hit.point, quaternion.RotateY(Random.Range(0f,180f)));
-                _spawnedPrefabs.Add(o);
-            }
-        }
+        GUIUtility.hotControl = 1;
+        int randomChoice = Random.Range(0, _prefabChoiceLst.Count);
+        //Positionen speichern und vergleichen
+        GameObject o = Instantiate(_prefabChoiceLst[randomChoice], hit.point + new Vector3(Random.insideUnitCircle.x * _radius, 0, Random.insideUnitCircle.y * _radius), quaternion.RotateY(Random.Range(0f,180f)));
+        o.GetComponent<MeshRenderer>().sharedMaterial.enableInstancing = true;
+        _spawnedPrefabs.Add(o);
+        
     }
 
     private void DrawReorderableList<T>(List<T> sourceList, VisualElement rootVisElement, bool allowSceneObjects = true) where T : UnityEngine.Object
